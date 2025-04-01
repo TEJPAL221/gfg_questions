ec8b81fb17483dcf80901de2a0b24fbc1224020f
TEJPAL221 <126184577+TEJPAL221@users.noreply.github.com>
2025-04-01 12:40:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete in a Doubly Linked List/delete-in-a-doubly-linked-list.java
@@ -0,0 +1,127 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
+
+public class Main {
+    // Function to insert a node at the end of the Doubly Linked List
+    public static void push(Node tail, int new_data) {
+        Node newNode = new Node(new_data);
+        newNode.next = null;
+        newNode.prev = tail;
+
+        if (tail != null) {
+            tail.next = newNode;
+        }
+    }
+
+    // Function to print nodes in a given doubly linked list
+    public static void printList(Node head) {
+        if (head == null) {
+            return;
+        }
+        if (!verify(head)) {
+            return;
+        }
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // Main driver function
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String[] arr = br.readLine().trim().split(" ");
+
+            Node head = new Node(Integer.parseInt(arr[0]));
+            Node tail = head;
+
+            for (int i = 1; i < arr.length; i++) {
+                push(tail, Integer.parseInt(arr[i]));
+                tail = tail.next;
+            }
+            int x = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            head = obj.deleteNode(head, x);
+            printList(head);
+            System.out.println("~");
+        }
+    }
+
+    // Verifying the doubly linked list
+    public static boolean verify(Node head) {
+        int forwardLength = 0;
+        int backwardLength = 0;
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+            forwardLength++;
+        }
+
+        while (temp.prev != null) {
+            temp = temp.prev;
+            backwardLength++;
+        }
+
+        return forwardLength == backwardLength;
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        // If the list is empty
+        if (head == null) return null;
+
+        // If deleting the head node
+        if (x == 1) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null; // Remove previous link
+            }
+            return head;
+        }
+
+        // Traverse to the (x-1)th node
+        Node temp = head;
+        for (int i = 1; i < x && temp != null; i++) {
+            temp = temp.next;
+        }
+
+        // If temp is null, position is out of bounds
+        if (temp == null) return head;
+
+        // If deleting the last node
+        if (temp.next == null) {
+            temp.prev.next = null;
+        } 
+        // Deleting a middle node
+        else {
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+        }
+
+        return head;
+    }
+}
